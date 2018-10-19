0a8c39647efbff4c90e222cc4a09af326f096654
theppsh <theppsh@example.com>
2018-10-19 16:41:21 +0800

    forward ok!

--- conv/conv2d.py
+++ conv/conv2d.py
@@ -37,42 +37,46 @@ class Conv2D:
         Return:
             x (th.tensor[float32]): [b,c_out,h_out,w_out]
         """
-        # [b,pach_len,patch_num]
-        patches=self._img2col(x)
+        b,c_in,h,w=x.shape
+        c_out=self.channels_out
+        k_size=self.kernel_size
+        # 1. change to matrix mul
+        # [b,c_in*k_size_h*k_size_w,num_patch], num_patch=nh*nw
+        patches,nh,nw=self._img2col(x)
+        num_patch=nh*nw
+        
+        # 2. w.mm(patches)+bias
+        weights=self.weights.view(-1,c_in*k_size[0]*k_size[1]) # [c_out,c_in*k_size[0]*k_size[1]]
+        bias=self.bias[None,:,None].expand(b,c_out,num_patch) # [b,c_out,num_patch]
+        x=weights @ patches + bias # [b,c_out,num_patch]
 
-        return patches
+        # 3. reshape
+        x=x.view(b,c_out,nh,nw)
+        return x
 
-    def backward(self,dx):
+    def backward(self,x,dx):
         r"""Backward function
         Args:
             dx (th.tensor[float32]): [b,c_out,h_out,w_out]
         Return:
             dx (th.tensor[float32]): [b,c_out,h_out,w_out] 
         """
+        return self._col2img(dx,x.shape)
     
-    def _img2col(self,x):
-        r"""Change imgaes patches to a matrix
+    def get_indices(self,h,w,k_size,stride,padding):
+        r""" Get the indices for the patches 
         Args:
-            x (th.tensor[float32]): [b,c,h,w]
+            h (int): height
+            w (int): width
+            k_size (tuple):
+            stride (tuple):
+            padding (tuple):
         Return:
-            x (th.tensor[float32]): [b,patches,c*kernel_h*kernek_w]  
+            steps_at_i (torch.tensor[long]):
+            steps_at_j (torch.tensor[long]):
+            index_at_i (torch.tensor[long]):
+            index_at_j (torch.tensor[long]):
         """
-        stride=self.stride
-        padding=self.padding
-        k_size=self.kernel_size
-        
-        b,c,h,w=x.shape
-        
-        assert (h+2*padding[0]-k_size[0]) % stride[0] ==0
-        assert (w+2*padding[1]-k_size[1]) % stride[1] ==0
-        if padding[0] >0 or padding[1] >0 :
-            padded_h=h+2*padding[0]
-            padded_w=w+2*padding[1]
-            padded_x=th.zeros(b,c,padded_h,padded_w).type_as(x)
-            padded_x[:,:,padding[0]:-padding[0],padding[1]:-padding[1]]=x
-        else:
-            padded_x=x
-
         # 1. counter in x_axis and y_axis, the output feature map size
         steps_at_i=(h+2*padding[0]-k_size[0])// stride[0]+1
         steps_at_j=(w+2*padding[1]-k_size[1])// stride[1]+1
@@ -108,28 +112,125 @@ class Conv2D:
         # [steps_at_i,steps_at_j,k_size[0]*k_size[1]]
         idx_at_j=idx_at_j.view(steps_at_i,steps_at_j,-1)
 
-        # 4. indexing
+        return steps_at_i,steps_at_j,idx_at_i,idx_at_j
+
+    def _pad(self,x,k_size,stride,padding):
+        b,c,h,w=x.shape
+        if padding[0] >0 or padding[1] >0 :
+            padded_h=h+2*padding[0]
+            padded_w=w+2*padding[1]
+            padded_x=th.zeros(b,c,padded_h,padded_w).type_as(x)
+            padded_x[:,:,padding[0]:-padding[0],padding[1]:-padding[1]]=x
+        else:
+            padded_x=x.clone()
+        
+        return padded_x
+
+    def _img2col(self,x):
+        r"""Change imgaes patches to a matrix
+        Args:
+            x (th.tensor[float32]): [b,c,h,w]
+        Return:
+            x (th.tensor[float32]): [b,c*k_size_h*k_size_w,num_patch]
+            steps_at_i (int): height of the generated feature map
+            steps_at_j (int): width of the generated feature map
+        """
+        stride=self.stride
+        padding=self.padding
+        k_size=self.kernel_size
+        
+        b,c,h,w=x.shape
+        
+        assert (h+2*padding[0]-k_size[0]) % stride[0] == 0
+        assert (w+2*padding[1]-k_size[1]) % stride[1] == 0
+        
+        # 1. pad zero 
+        padded_x=self._pad(x,k_size,stride,padding)
+
+        # 2. get indices on padded_x for patches
+        steps_at_i,\
+            steps_at_j,\
+            idx_at_i,idx_at_j=\
+            self.get_indices(h,w,k_size,stride,padding)
+
+        # 3. indexing
         # [b,c,steps_at_i,steps_at_j,k_size[0]*k_size[1]]
         patches=padded_x[...,idx_at_i,idx_at_j] 
         
-        # 5. permute and reshape
+        # 4. permute and reshape
         # [b,c*k_size[0]*k_size[1],steps_at_i*steps_at_j]
         patches=patches.permute(0,1,4,2,3).contiguous()\
             .view(b,c*k_size[0]*k_size[1],steps_at_i*steps_at_j)
         
-        
-        return patches
+        return patches,steps_at_i,steps_at_j
 
-    def _col2img(self,x):
+    def _col2img(self,patches,feat_shape):
         r"""Convert the patches to imgs by cumulative fasion
+        Args:
+            patches (th.tensor[float32]): [b,c*k_size[0]*k_size[1],steps_at_i*steps_at_j]
+            feat_shape (tuple): (b,c,h,w)
+        Return:
+            x (th.tensor[float32]): [b,c,h,w]
         """
+        stride=self.stride
+        padding=self.padding
+        k_size=self.kernel_size
+        b,c,h,w=feat_shape
+
+        # 1. allocate the memory
+        padded_h=h+2*padding[0]
+        padded_w=w+2*padding[1]
+        padded_x=th.zeros(b,c,padded_h,padded_w).type_as(patches) # [b,c,ph,pw]
+
+        # 2. get indices on padded_x for patches
+        steps_at_i,\
+            steps_at_j,\
+            idx_at_i,idx_at_j=\
+            self.get_indices(h,w,k_size,stride,padding)
+
+        # 3. for `index_sum`
+        padded_x=padded_x.permute(2,3,0,1).contiguous() # [ph,pw,b,c]
+        padded_x=padded_x.view(-1,b,c) # [ph*pw,b,c]
+    
+        indices=idx_at_i*padded_w+idx_at_j # [steps_at_i,steps_at_j,k_size[0]*k_size[1]]
+        indices=indices.view(-1) # [steps_at_i*steps_at_j*k_size[0]*k_size[1]]
+        
+        # [b,c,k_size[0]*k_size[1],steps_at_i,steps_at_j]
+        patches=patches.view(b,c,k_size[0]*k_size[1],steps_at_i,steps_at_j)
+        # [steps_at_i,steps_at_j,k_size[0]*k_size[1],b,c]
+        patches=patches.permute(3,4,2,0,1).contiguous()
+        # [steps_at_i*steps_at_j*k_size[0]*k_size[1],b,c]
+        patches=patches.view(-1,b,c)
+        
+        # 4. cumulative add...
+        # NOTE: different from `padded_x[indices]+=patches`
+        padded_x.index_add_(0,indices,patches)
+
+        # 5. convert to original shape
+        # [ph,pw,b,c]
+        padded_x=padded_x.view(padded_h,padded_w,b,c)
+        # [b,c,ph,pw]
+        padded_x=padded_x.permute(2,3,0,1).contiguous()
+
+        # 6. clip
+        padded_x =padded_x[...,padding[0]:-padding[0],padding[1]:-padding[1]] # [b,c,h,w]
+
+        return padded_x
 
 
 def main():
     x=th.arange(2*3*3*6).view(2,3,3,6)
-    patches=Conv2D(3,5,(3,1),(2,1),(1,1))(x)
+    m=Conv2D(3,5,(3,1),(2,1),(1,1))
+    m2=th.nn.Conv2d(3,5,(3,1),(2,1),padding=(1,1))
+    m2.weight.data.copy_(m.weights)
+    m2.bias.data.copy_(m.bias)
+    patches=m(x)
+    patches2=m2(x)
     print(x)
     print(patches)
+    
+    # cum_add=m.backward(x,patches)
+    # print(cum_add)
 
 if __name__ == '__main__':
     main()
